38b22ed6f5d97c7f7e117bc07f290ffc826215be
oosmoxiecode <klas@northkingdom.com>
2012-05-28 20:41:02 +0200

    Updated builder.py

--- utils/builder.py
+++ utils/builder.py
@@ -3,7 +3,7 @@ import os
 rev = 0
 source = '../src/xgui.js'
 build = '../build/xgui.min.js'
-header = '// xgui.js r' + str( rev ) + ' - @author oosmoxiecode - http://oos.moxiecode.com/\n'
+header = '// xgui.js r' + str( rev ) + ' - https://github.com/oosmoxiecode/xgui.js\n'
 
 os.system( 'java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + build )
 
